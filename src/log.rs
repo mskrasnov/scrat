0800ecbee74cf96bcf8e857614fa5f844e855641
mskrasnov <linuxoid85@gmail.com>
2025-01-05 22:29:09 +0300

    Getting started on logging

---
+++ src/log.rs
@@ -0,0 +1,48 @@
+//! Simple logging
+
+use serde::{Serialize, Deserialize};
+use toml;
+use std::{fs, path::Path};
+use color_eyre::Result;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Log {
+    #[serde(rename = "log")]
+    pub logs: Vec<LogEntry>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LogEntry {
+    #[serde(rename = "type")]
+    pub log_type: LogType,
+    pub ctime: i64,
+    pub message: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub enum LogType {
+    Debug,
+    #[default]
+    Info,
+    Warning,
+    Error,
+    CriticalError,
+}
+
+impl Log {
+    pub fn read<P: AsRef<Path>>(pth: P) -> Result<Self> {
+        let contents = fs::read_to_string(&pth)?;
+        Ok(toml::from_str(&contents)?)
+    }
+
+    pub fn write<P: AsRef<Path>>(&self, pth: P) -> Result<()> {
+        let contents = toml::to_string(&self)?;
+        fs::write(&pth, contents)?;
+
+        Ok(())
+    }
+
+    pub fn add_entry(&mut self, entry: LogEntry) {
+        self.logs.push(entry);
+    }
+}
