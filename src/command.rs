214991fddeb6918d5d81302e6142389dc458cc0a
mskrasnov <linuxoid85@gmail.com>
2025-01-05 23:34:12 +0300

    Add some shit

--- src/command.rs
+++ src/command.rs
@@ -6,16 +6,49 @@
 //! :<COMMAND> <SUBCOMMAND> <ARG 1> <ARG 2> ... <ARG N>
 //! ```
 
-#[derive(Debug, Clone)]
-pub enum MasterCommand {
-    Home,
-    PostNew,
-    DeletePost,
+use color_eyre::Result;
+use std::{fmt::Display, io::{stdin, stdout, Write}, str::FromStr};
+
+pub fn prompt<M: Display>(msg: M) -> Result<String> {
+    print!("{msg}> ");
+    stdout().flush()?;
+
+    let mut s = String::new();
+    stdin().read_line(&mut s)?;
+
+    Ok(s.trim().to_string())
 }
 
 #[derive(Debug, Clone)]
-pub struct PostNewCommand {
-    pub status: String,
-    pub sensitive: Option<bool>,
-    pub language: Option
+pub enum Command {
+    NewStatus,
+    StatusList,
+
+    Help,
+    Exit,
+
+    ErrorCmd,
+}
+
+impl FromStr for Command {
+    type Err = ();
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        Ok(match s {
+            "new-status" | "status" => Self::NewStatus,
+            "status-list" => Self::StatusList,
+            "help" => Self::Help,
+            "exit" => Self::Exit,
+
+            _ => Self::ErrorCmd,
+        })
+    }
+}
+
+impl Command {
+    pub fn print_help() {
+        println!("{:>width$}: {}", "new-status | status", "write new status", width = 25);
+        println!("{:>width$}: {}", "status-list", "print 5 my statuses", width = 25);
+        println!("{:>width$}: {}", "exit", "exit program", width = 25);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 //pub mod command;
+pub mod command;
 pub mod consts;
 pub mod log;
 
 pub mod register;
 
-//use std::time::Duration;
+use futures_util::StreamExt;
+use mastodon_async::{prelude::*, Error, Result, StatusesRequest};
+use std::str::FromStr;
 
-use mastodon_async::{prelude::*, Result};
+async fn create_new_status(md: &Mastodon) -> Result<()> {
+    let status_txt =
+        command::prompt("Enter your status").map_err(|err| Error::Other(err.to_string()))?;
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let md = register::get_mastodon_data("./md.toml", "https://mastodon.ml").await?;
     let status = StatusBuilder::default()
-        .status("TEST STATUS")
+        .status(status_txt)
         .visibility(Visibility::Public)
         .language(mastodon_async::Language::Rus)
         .build()?;
@@ -24,6 +26,54 @@ async fn main() -> Result<()> {
     } else {
         println!("For some reason, the status URL was not returned from the server :(");
     }
+    Ok(())
+}
+
+async fn get_statuses(md: &Mastodon) -> Result<()> {
+    let mut filters = StatusesRequest::new();
+    filters.limit(5);
+
+    let me = md.verify_credentials().await?;
+
+    md.statuses(&AccountId::new(me.id.as_ref()), filters)
+        .await?
+        .items_iter()
+        .for_each(|status| async move {
+            let created = &status.created_at;
+            println!(
+                "Status created: {}, {}:{}",
+                created.year(),
+                created.hour(),
+                created.minute(),
+            );
+            println!("{}\n", &status.content);
+        })
+        .await;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let md = register::get_mastodon_data("./md.toml", "https://mastodon.ml").await?;
+    let mut exit = false;
+
+    while !exit {
+        let cmd = command::Command::from_str(
+            &command::prompt("").map_err(|err| Error::Other(err.to_string()))?,
+        )
+        .map_err(|_| Error::Other("Unknown command".to_string()))?;
+
+        match cmd {
+            command::Command::NewStatus => create_new_status(&md).await?,
+            command::Command::StatusList => get_statuses(&md).await?,
+            command::Command::Help => command::Command::print_help(),
+            command::Command::Exit => exit = true,
+            _ => {}
+        }
+    }
+
+    println!("Have a good day.");
 
     Ok(())
 }
