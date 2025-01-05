8538e30b3cb797c7eeb8417356f00522857a805a
mskrasnov <linuxoid85@gmail.com>
2025-01-05 22:29:28 +0300

    Use build-in consts

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 //pub mod command;
 pub mod consts;
+pub mod log;
+
 pub mod register;
 
 //use std::time::Duration;
@@ -8,7 +10,7 @@ use mastodon_async::{prelude::*, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let md = register::get_mastodon_data("./md.toml", "https://mastodon.md", consts::CLIENT_SITE).await?;
+    let md = register::get_mastodon_data("./md.toml", "https://mastodon.ml").await?;
     let status = StatusBuilder::default()
         .status("TEST STATUS")
         .visibility(Visibility::Public)
@@ -23,115 +25,5 @@ async fn main() -> Result<()> {
         println!("For some reason, the status URL was not returned from the server :(");
     }
 
-    /*let mut i = 10;
-    while i > 0 {
-        println!("{i} secs...");
-        std::thread::sleep(Duration::from_secs(i));
-        i -= 1;
-    }
-    
-    md.delete_status(status).await?;*/
-
     Ok(())
 }
-
-/*use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
-use ratatui::{
-    buffer::Buffer,
-    layout::Rect,
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{Block, Paragraph, Widget},
-    DefaultTerminal, Frame,
-};
-use std::io;
-
-fn main() -> io::Result<()> {
-    let mut term = ratatui::init();
-    let app = App::default().run(&mut term);
-    ratatui::restore();
-    app
-}
-
-#[derive(Debug, Default)]
-struct App {
-    counter: u8,
-    exit: bool,
-}
-
-impl App {
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.draw(frame))?;
-            self.handle_events()?;
-        }
-        Ok(())
-    }
-
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
-        };
-        Ok(())
-    }
-
-    fn handle_key_event(&mut self, event: KeyEvent) {
-        match event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Left => self.decrement_cnt(),
-            KeyCode::Right => self.increment_cnt(),
-            _ => {}
-        }
-    }
-
-    fn exit(&mut self) {
-        self.exit = true;
-    }
-
-    fn increment_cnt(&mut self) {
-        self.counter += 1;
-    }
-
-    fn decrement_cnt(&mut self) {
-        self.counter -= 1;
-    }
-}
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer)
-    where
-        Self: Sized,
-    {
-        let title = Line::from(" Scrat Mastodon Client ".bold());
-        let instructions = Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
-
-        let cnt_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.counter.to_string().yellow(),
-        ])]);
-
-        Paragraph::new(cnt_text)
-            .centered()
-            .block(block)
-            .render(area, buf);
-    }
-}*/
--- src/register.rs
+++ src/register.rs
@@ -5,28 +5,29 @@ use mastodon_async::{
 };
 use std::path::Path;
 
+use crate::consts::{PROGRAM_NAME, CLIENT_SITE};
+
 pub async fn get_mastodon_data<P: AsRef<Path>>(
     pth: P,
     instance_url: &str,
-    client_url: &str,
 ) -> Result<Mastodon> {
     match toml::from_file(&pth) {
         Ok(data) => Ok(Mastodon::from(data)),
-        Err(_) => register(&pth, instance_url, client_url).await,
+        Err(_) => register(&pth, instance_url).await,
     }
 }
 
 pub async fn register<P: AsRef<Path>>(
     pth: P,
     instance_url: &str,
-    client_url: &str,
 ) -> Result<Mastodon> {
     let reg = Registration::new(instance_url)
-        .client_name("Scrat")
+        .client_name(PROGRAM_NAME)
         .scopes(Scopes::all())
-        .website(client_url)
+        .website(CLIENT_SITE)
         .build()
         .await?;
+
     let mastodon = cli::authenticate(reg).await?;
 
     toml::to_file(&mastodon.data, &pth)?;
